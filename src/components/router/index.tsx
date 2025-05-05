a3c95bb0c8463f1f9dd764246bc09ef4dde40af0
Abhishek Kalia <abhishek.kalia@indianic.com>
2025-05-05 19:39:06 +0530

    ui override

---
+++ src/components/router/index.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+type TNavLink = {
+    children: any;
+    to: any;
+    tag: any;
+    className?: string;
+    activeStyle?: any;
+    style?: any;
+    onMouseEnter?: any;
+    onMouseOut?: any;
+}
+
+
+const NavLink = ({ children, to, tag, className, activeStyle, style, ...rest }: TNavLink) => {
+    return (
+        <Link href={to} passHref legacyBehavior {...rest} >
+            <div className={className} style={{ ...activeStyle, ...style }}>
+                {children}
+            </div>
+        </Link>
+    )
+}
+
+export default NavLink
---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Header from "../components/header/Header";
+import Greeting from "../containers/greeting/Greeting";
+import Skills from "../containers/skills/Skills";
+import Footer from "../components/footer/Footer";
+import TopButton from "../components/topButton/TopButton";
+import { chosenTheme } from "./theme";
+
+const Home = () => {
+  return (
+    <div>
+      <Header theme={chosenTheme} />
+      <Greeting theme={chosenTheme} />
+      <Skills theme={chosenTheme} />
+      <Footer theme={chosenTheme} />
+      <TopButton theme={chosenTheme} />
+    </div>
+  );
+}
+
+export default Home;
